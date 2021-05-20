c6f9ef5863627b1037d26769d6b567cee1046c28
sbloxy123 <sbloxy123@gmail.com>
2021-05-20 20:05:03 +0100

    change grid layout
    from 3 fr to 2fr @768px

--- src/components/FeatureProducts.js
+++ src/components/FeatureProducts.js
@@ -1,8 +1,10 @@
 import React from "react";
-import styled from "styled-components";
 import { SubTitle } from "../components/styles/Styles";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { graphql, useStaticQuery } from "gatsby";
+import { ImageLayout } from "../components/styles/GridLayout";
+
+// featured products on home page
 
 const query = graphql`
   {
@@ -26,53 +28,27 @@ const FeatureProducts = () => {
   const nodes = data.allFile.nodes;
 
   return (
-    <Products>
+    <div>
       <SubTitle>New Arrivals</SubTitle>
-      <div className="product-images">
-        {nodes.map((image, index) => {
-          const pathToImage = getImage(image);
-          return (
-            <GatsbyImage
-              image={pathToImage}
-              alt="hello"
-              key={index}
-              className="product-image"
-            />
-          );
-        })}
-      </div>
-    </Products>
+      <ImageLayout>
+        <div>
+          <div className="collection-cards">
+            {nodes.map((image, index) => {
+              const pathToImage = getImage(image);
+              return (
+                <GatsbyImage
+                  image={pathToImage}
+                  alt="hello"
+                  key={index}
+                  className="collection"
+                />
+              );
+            })}
+          </div>
+        </div>
+      </ImageLayout>
+    </div>
   );
 };
-
-const Products = styled.section`
-  text-align: center;
-  margin: 3rem auto;
-  max-width: 1170px;
-
-  h2 {
-    font-family: var(--styled-font);
-    font-size: 4rem;
-    margin-bottom: 0rem;
-  }
-
-  .product-images {
-    display: inline-grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
-
-    .product-image {
-      margin: 0.5rem;
-    }
-  }
-  @media screen and (max-width: 768px) {
-    h2 {
-      margin: 0rem;
-    }
-    .product-images {
-      grid-template-columns: 1fr;
-    }
-  }
-`;
 
 export default FeatureProducts;
--- src/components/styles/GridLayout.js
+++ src/components/styles/GridLayout.js
@@ -43,14 +43,14 @@ export const ImageLayout = styled.div`
       }
     }
   }
-  @media screen and (max-width: 768px) {
+  /* @media screen and (max-width: 768px) {
     .collection-cards {
       grid-template-columns: 1fr 1fr;
       .collection {
         margin-top: 1rem;
       }
     }
-  }
+  } */
   @media screen and (max-width: 501px) {
     .collection-cards {
       grid-template-columns: 1fr;
